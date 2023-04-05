0ad51cb3908f4edaed550c6bfe570d343ba9d2fa
sumitshinde-84 <shindesumit217@gmail.com>
2023-04-05 22:08:00 +0530

    add updateWeb function

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-async function getData(location = "mumbai") {
+const search = document.querySelector('#search');
+const tempreturePara = document.querySelector('.temprature-para')
+const wheatherIcon = document.querySelector('#wheather-icon')
+
+async function fetchWetherData(location = "mumbai") {
   try {
     let responce = await fetch(
       `https://api.weatherapi.com/v1/current.json?key=3a5327c8a4ab4fa3b34110849230304&q=${location}`,
@@ -11,10 +15,26 @@ async function getData(location = "mumbai") {
   }
 }
 
-getData().then((data) => {
-    console.log(data)
-  let img = document.createElement('img')
-  let body = document.querySelector('body')
-  img.src = data.current.condition.icon
-  body.appendChild(img)
+document.addEventListener('keypress', function (e) {
+  if (e.keyCode === 13 || e.which === 13) {
+    updatePage(search.value)
+   }
+
 });
+
+function updatePage(location='Mumbai'){
+  fetchWetherData(location).then((data) => {
+    console.log(data)
+    updateWeb(data)
+});
+
+}
+
+
+function updateWeb(data){
+ tempreturePara.innerHTML=`${data.current['temp_c']}<span>°C</span>`
+  wheatherIcon.src=`${data.current.condition.icon}`
+}
+
+
+updatePage()
